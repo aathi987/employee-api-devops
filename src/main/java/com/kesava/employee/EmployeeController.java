afc4a0ed36da8b39f8a8ed82ddbffce0e76dc533
aathi987 <aathikesavan987@gmail.com>
2026-09-09 21:49:05 +0530

    Add Controller

--- src/main/java/com/kesava/employee/EmployeeController.java
+++ src/main/java/com/kesava/employee/EmployeeController.java
@@ -1,15 +1,12 @@
 package com.kesava.employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
-@RestController
-public class EmployeeController {
-    @GetMapping("/api/employees")
-    public List<Employee> getEmployees() {
-        return List.of(
-            new Employee(1, "Kesavaraja"),
-            new Employee(2, "DevOps Engineer")
-        );
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
     }
 }
