2f297236a765c6490267b258edbe5a0b3b969fa6
aathi987 <aathikesavan987@gmail.com>
2026-09-10 10:26:50 +0530

    Implement EmployeeController with CRUD operations
    Refactored HomeController to EmployeeController with RESTful endpoints for employee management.

--- src/main/java/com/kesava/employee/EmployeeController.java
+++ src/main/java/com/kesava/employee/EmployeeController.java
@@ -1,12 +1,46 @@
 package com.kesava.employee;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "forward:/index.html";
+@RestController
+@RequestMapping("/api/employees")
+@CrossOrigin(origins = "*")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee != null) {
+            employee.setName(employeeDetails.getName());
+            employee.setEmail(employeeDetails.getEmail());
+            employee.setDepartment(employeeDetails.getDepartment());
+            return employeeRepository.save(employee);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
     }
 }
