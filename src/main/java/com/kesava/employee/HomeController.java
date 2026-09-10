642fb0aec98952baad3f5902379da3976a97f24a
aathi987 <aathikesavan987@gmail.com>
2026-09-10 10:29:43 +0530

    Add files via upload

---
+++ src/main/java/com/kesava/employee/HomeController.java
@@ -0,0 +1,8 @@
+package com.kesava.employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() { return "forward:/index.html"; }
+}
