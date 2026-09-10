6daae97007e99fe12b27189efcf2982908491f18
aathi987 <aathikesavan987@gmail.com>
2026-09-10 11:46:12 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee (name, email, department) VALUES ('Aathi Kesava', 'aathi@devops.com', 'DevOps Engineer');
+INSERT INTO employee (name, email, department) VALUES ('John Doe', 'john@company.com', 'IT Department');
+INSERT INTO employee (name, email, department) VALUES ('Priya Sharma', 'priya@company.com', 'HR');
+INSERT INTO employee (name, email, department) VALUES ('Raj Kumar', 'raj@company.com', 'Backend Developer');
